d89bd7cfd51de4ccfa79ef270f327dd664886b60
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-02 12:00:05 +0100

    Create `mySampleClass`

---
+++ src/base/MySampleClass.java
@@ -0,0 +1,11 @@
+package base;
+
+public class MySampleClass {
+    public static final double PI = 3.14159265358979323846;
+    public Integer myNumber;
+    private boolean isOk;
+
+    public MySampleClass(Integer myNumber) {
+        this.myNumber = myNumber;
+    }
+}
