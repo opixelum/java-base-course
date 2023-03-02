1aa9b571f6e98380b36d46b0be7f53689950072a
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-02 16:06:05 +0100

    Define `Point` class

--- src/base/Main.java
+++ src/base/Main.java
@@ -2,11 +2,8 @@ package base;
 
 public class Main {
     public static void main(String[] args) {
-        // Loop for creating 5 instances of MySampleClass
-        for (int i = 0; i < 5; i++) {
-            MySampleClass sample = new MySampleClass(i);
-            sample.setOk(true);
-            System.out.println("My number is " + sample.myNumber + " and isOk is " + sample.isOk() + ".");
-        }
+        // Define a point
+        Point p = new Point(1, 2, 3);
+        System.out.println(p.getX() + ", " + p.getY() + ", " + p.getZ());
     }
 }
---
+++ src/base/Point.java
@@ -0,0 +1,49 @@
+package base;
+
+public class Point {
+    private int x;
+    private int y;
+    private int z;
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point(int x, int y) {
+        this(x, y, 0);
+    }
+
+    public Point(int x) {
+        this(x, 0, 0);
+    }
+
+    public Point() {
+        this(0, 0, 0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
