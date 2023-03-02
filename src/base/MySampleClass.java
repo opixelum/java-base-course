7605beb4aee3b0d67e9944f8dda6452167bac98d
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-02 12:07:33 +0100

    Add comments

--- src/base/MySampleClass.java
+++ src/base/MySampleClass.java
@@ -1,10 +1,16 @@
 package base;
 
 public class MySampleClass {
+    // Constant
     public static final double PI = 3.14159265358979323846;
+
+    // Type is a class
     public Integer myNumber;
+
+    // Variable only accessible within the class
     private boolean isOk;
 
+    // Constructor
     public MySampleClass(Integer myNumber) {
         this.myNumber = myNumber;
     }
