1a491849b506ecd43a448befd4bd32013110acaf
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-02 12:35:29 +0100

    Create 5 instances of `MySampleClass`

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,2 +1,12 @@
-package base;public class Main {
+package base;
+
+public class Main {
+    public static void main(String[] args) {
+        // Loop for creating 5 instances of MySampleClass
+        for (int i = 0; i < 5; i++) {
+            MySampleClass sample = new MySampleClass(i);
+            sample.setOk(true);
+            System.out.println("My number is " + sample.myNumber + " and isOk is " + sample.isOk() + ".");
+        }
+    }
 }
